8cd3ac42f72ea232feed1704daa2894c89880500
Ivan <pacheco.gonzalez.ivan@alumnat.copernic.cat>
2022-03-07 12:51:52 +0100

    Añadiendo estructura del proyecto

--- src/main/java/cat/copernic/erpInsCavallBernat/ErpInsCavallBernatApplication.java
+++ src/main/java/cat/copernic/erpInsCavallBernat/ErpInsCavallBernatApplication.java
@@ -1,6 +1,5 @@
 package cat.copernic.erpInsCavallBernat;
 
-import static java.sql.DriverManager.println;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
