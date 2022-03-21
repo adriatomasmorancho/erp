0f094d29fc889bb2df6f2a29f0fe47a179c3cce3
Adria Tomas <tomas.morancho.adria@alumnat.copernic.cat>
2022-03-21 18:51:01 +0100

    Actualització

--- src/main/java/cat/copernic/erpInsCavallBernat/model/Categoria.java
+++ src/main/java/cat/copernic/erpInsCavallBernat/model/Categoria.java
@@ -20,15 +20,15 @@ import lombok.Data;
  */
 @Data
 @Entity
-@Table(name = "producte")
-public class Producte implements Serializable {
+@Table(name = "categoria")
+public class Categoria implements Serializable {
 
     //Identificació de la classe per poder deserialitzar de manera correcta
     private static final long serialVersionUID = 1L;
 
     @Id //Indica al sistema que l'atribut idgos és la clau primària de la BBDD
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Indica al sistema com generarem l'id
-    private long id_Producte;
+    private long id_Categoria;
     /*Validació per comprovar que el nom no està buit. Com a paràmetre li passem el missatge
      *que volem que aparegui.
      */
@@ -37,20 +37,6 @@ public class Producte implements Serializable {
     /*Validació per comprovar que el sexe no està buit. Com a paràmetre no li passem res, per tant
      *ens mostrarà el missatge per defecte del sitema.
      */
-    @NotEmpty
-    private String unitat;
-    
-    @NotEmpty
-    private double preu;
-    
-    @NotEmpty
-    private String categoria;
-    
-    //FALTA STOCK
-    //@NotEmpty
-    //private String stock;
-    
-    @NotEmpty
-    private String cifProveidor;
+   
   
 }
